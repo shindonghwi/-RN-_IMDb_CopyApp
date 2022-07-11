339bbaf482d0715f2cfc8cbf12ab9ccf5f8a9bb1
shindonghwi <ehdgnl8940@naver.com>
2022-07-11 13:23:17 +0900

    feat: search movie, tv api add

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components/native";
+import { useQuery } from "react-query";
+import { moviesApi, tvApi } from "../api";
 
 const Container = styled.ScrollView``;
 const SearchBar = styled.TextInput`
@@ -12,14 +14,38 @@ const SearchBar = styled.TextInput`
 
 const Search = () => {
     const [query, setQuery] = useState("");
+    const {
+        isLoading: movieLoading,
+        data: movieData,
+        refetch: searchMovies,
+    } = useQuery(["searchMovies", query], moviesApi.search, {
+        enabled: false,
+    });
+    const {
+        isLoading: tvLoading,
+        data: tvData,
+        refetch: searchTv,
+    } = useQuery(["searchTv", query], tvApi.search, {
+        enabled: false,
+    });
     const onChangeText = (text: string) => setQuery(text);
+
+    const onSubmit = async () => {
+        if (query === "") {
+            return;
+        }
+        await searchMovies();
+        await searchTv();
+    };
+
     return (
         <Container>
             <SearchBar
-                returnKeyType={"search"}
-                placeholder={"Search for Movie or TV Show"}
-                placeholderTextColor={"grey"}
+                returnKeyType="search"
+                placeholder="Search for Movie or TV Show"
+                placeholderTextColor="grey"
                 onChangeText={onChangeText}
+                onSubmitEditing={onSubmit}
             />
         </Container>
     );
